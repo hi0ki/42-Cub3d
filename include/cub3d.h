2f289526c00482e27f2b563bb2bfdc845af29a1d
ADAMkenaoui12 <kenaouiadam8@gmail.com>
2024-09-08 17:13:21 +0100

    vr1

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: kadam <kadam@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 12:12:37 by kadam             #+#    #+#             */
-/*   Updated: 2024/09/06 16:58:26 by kadam            ###   ########.fr       */
+/*   Updated: 2024/09/08 17:10:56 by kadam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,54 +19,61 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <math.h>
 #include "../gnl/get_next_line.h"
 #include "../MLX42/include/MLX42/MLX42.h"
 
+#define ROTATION_SPEED 0.5
+#define MOVE_SPEED 10.0
+#define Size 32
 typedef struct s_player
 {
 	int x;
 	int y;
-}t_player;
+	float px; // Player's x position
+	float py; // Player's y position
+	int player_dir; // 0 = N, 1 = E, 2 = S, 3 = W
+	mlx_image_t *image_player;
+	float angle;
+} t_player;
 
 typedef struct s_map
 {
-	//read the all map
-	char    **str;
-	//read the map
-	char	**map_array;
-	//read the path
-	char    *NO;
-	char    *SO;
-	char    *WE;
-	char    *EA;
-	//read the color
-	int     F[3];
-	int     C[3];
-	//for player
+	// read the all map
+	char **str;
+	// read the map
+	char **map_array;
+	// read the path
+	char *NO;
+	char *SO;
+	char *WE;
+	char *EA;
+	// read the color
+	int F[3];
+	int C[3];
+	// for player
 	t_player player;
-
 	mlx_t *mlx;
 	mlx_image_t *image;
-	int player_dir; // 0 = N, 1 = E, 2 = S, 3 = W
-}	t_map;
+} t_map;
 
 int ft_lenarray(char **array);
 void free_2d_array(char **array);
-void	ft_putstrn_fd(char *s, int fd);
-char	**ft_split(char *s, char *c);
-char	*ft_substr(char const *s, size_t start, size_t len);
-char	*ft_strchr(char *s, int c);
+void ft_putstrn_fd(char *s, int fd);
+char **ft_split(char *s, char *c);
+char *ft_substr(char const *s, size_t start, size_t len);
+char *ft_strchr(char *s, int c);
 char *ft_strtrim(char const *s1, char const *set);
 int ft_strcmp(const char *s1, const char *s2);
 int ft_atoi(const char *str);
-char	*_strjoin(char *s1, char *s2);
-int     _strlen(char *str);
+char *_strjoin(char *s1, char *s2);
+int _strlen(char *str);
 /*                    parsing                      */
 int check_all(int ac, char **av, t_map *map_struct, int fd);
 /*                      utils                   */
-void	check_map(t_map *s_map);
-char	**copy_arr(char **array);
-void	put_err(char *str, t_map *s_map);
+void check_map(t_map *s_map);
+char **copy_arr(char **array);
+void put_err(char *str, t_map *s_map);
 /*				check_utils						*/
 int valid_char(char **arr, t_map *s_map);
 int found_zero_index(char **arr, t_map *s_map);
--- main.c
+++ main.c
@@ -6,133 +6,184 @@
 /*   By: kadam <kadam@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 12:46:15 by kadam             #+#    #+#             */
-/*   Updated: 2024/09/06 20:38:02 by kadam            ###   ########.fr       */
+/*   Updated: 2024/09/08 17:13:03 by kadam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/cub3d.h"
 
+
 void draw_square(void *image, int x, int y, int size, int color)
 {
-	int i = 0;
-	int j = 0;
-	while (i < size)
-	{
-		j = 0;
-		while (j < size)
-		{
-			mlx_put_pixel(image, x + i, y + j, color);
-			j++;
-		}
-		i++;
-	}
+    int i = 0, j = 0;
+    while (i < size)
+    {
+        j = 0;
+        while (j < size)
+        {
+            mlx_put_pixel(image, x + i, y + j, color);
+            j++;
+        }
+        i++;
+    }
+}
+
+void draw_border(void *image, int x, int y, int size, int color)
+{
+    int i = 0, j = 0;
+    while (i < size)
+    {
+        mlx_put_pixel(image, x + i, y, color);
+        mlx_put_pixel(image, x + i, y + size - 1, color);
+        i++;
+    }
+    while (j < size)
+    {
+        mlx_put_pixel(image, x, y + j, color);
+        mlx_put_pixel(image, x + size - 1, y + j, color);
+        j++;
+    }
+}
+
+void draw_line(void *image, int x, int y, int size, int color, float angle)
+{
+    int y1 = 0, x1 = 0;
+    for (int i = 0; i < size; i++)
+    {
+        y1 = y + i * sin(angle);
+        x1 = x + i * cos(angle);
+        mlx_put_pixel(image, x1, y1, color);
+    }
 }
 
 void draw_map(t_map *map)
 {
-	int x = 0, y = 0;
-	while (map->map_array[y])
-	{
-		x = 0;
-		while (map->map_array[y][x])
-		{
-			if (ft_strchr("WESN10", map->map_array[y][x]))
-				draw_square(map->image, x * 32, y * 32, 32, 0xD3D3D3);
-			x++;
-		}
-		y++;
-	}
-	x = 0, y = 0;
-	while (map->map_array[y])
-	{
-		x = 0;
-		while (map->map_array[y][x])
-		{
-			if (map->map_array[y][x] == '1')
-				draw_square(map->image, x * 32, y * 32, 32, 0xFFFFFF);
-			else if (ft_strchr("WESN", map->map_array[y][x]))
-				draw_square(map->image, x * 32 + 8, y * 32 + 8, 16, 0xFF0000FF);
-			x++;
-		}
-		y++;
-	}
-}
-
-void move_player_and_draw(t_map *map, int p_x, int p_y, char c, int x)
-{
-	if (map->map_array[p_y][p_x] != '1')
-	{
-		map->map_array[map->player.y][map->player.x] = '0';
-		draw_square(map->image, map->player.x * 32, map->player.y * 32, 32, 0xD3D3D3);
-		if (x == 1)
-			map->player.y--;
-		else if (x == 2)
-			map->player.y++;
-		else if (x == 3)
-			map->player.x--;
-		else if (x == 4)
-			map->player.x++;
-		map->map_array[map->player.y][map->player.x] = c;
-		draw_square(map->image, map->player.x * 32 + 8, map->player.y * 32 + 8, 16, 0xFF0000FF);
-	}
+    int x = 0, y = 0;
+    while (map->map_array[y])
+    {
+        x = 0;
+        while (map->map_array[y][x])
+        {
+            if (ft_strchr("WESN10", map->map_array[y][x]))
+            {
+                draw_square(map->image, x * Size, y * Size, Size, 0xFFFFFFFF);
+                draw_border(map->image, x * Size, y * Size, Size, 0x000000FF);
+            }
+            x++;
+        }
+        y++;
+    }
+    x = 0;
+    y = 0;
+    while (map->map_array[y])
+    {
+        x = 0;
+        while (map->map_array[y][x])
+        {
+            if (map->map_array[y][x] == '1')
+                draw_square(map->image, x * Size, y * Size, Size, 0x000000FF);
+            else if (ft_strchr("WESN", map->map_array[y][x]))
+            {
+                mlx_put_pixel(map->player.image_player, x * Size, y * Size, 0xFF0000FF);
+                draw_line(map->player.image_player, x * Size, y * Size, 16, 0xFF0000FF, map->player.angle);
+            }
+            x++;
+        }
+        y++;
+    }
 }
 
 void key_hook(mlx_key_data_t keydata, void *param)
 {
-	t_map *map = (t_map *)param;
-	if (keydata.key == MLX_KEY_W && keydata.action)
-		move_player_and_draw(map, map->player.x, map->player.y - 1, 'N', 1);
-	else if (keydata.key == MLX_KEY_S && keydata.action)
-		move_player_and_draw(map, map->player.x, map->player.y + 1, 'N', 2);
-	else if (keydata.key == MLX_KEY_A && keydata.action)
-		move_player_and_draw(map, map->player.x - 1, map->player.y, 'N', 3);
-	else if (keydata.key == MLX_KEY_D && keydata.action)
-		move_player_and_draw(map, map->player.x + 1, map->player.y, 'N', 4);
-	if (keydata.key == MLX_KEY_ESCAPE)
-		exit(1);
+    t_map *map = (t_map *)param;
+    float move_speed = MOVE_SPEED;
+    float new_px = map->player.px;
+    float new_py = map->player.py;
+
+    if (keydata.key == MLX_KEY_W && keydata.action)
+    {
+        new_px += cos(map->player.angle) * move_speed;
+        new_py += sin(map->player.angle) * move_speed;
+    }
+    else if (keydata.key == MLX_KEY_S && keydata.action)
+    {
+        new_px -= cos(map->player.angle) * move_speed;
+        new_py -= sin(map->player.angle) * move_speed;
+    }
+    else if (keydata.key == MLX_KEY_A && keydata.action)
+    {
+        new_px -= cos(map->player.angle + M_PI_2) * move_speed;
+        new_py -= sin(map->player.angle + M_PI_2) * move_speed;
+    }
+    else if (keydata.key == MLX_KEY_D && keydata.action)
+    {
+        new_px += cos(map->player.angle + M_PI_2) * move_speed;
+        new_py += sin(map->player.angle + M_PI_2) * move_speed;
+    }
+    else if (keydata.key == MLX_KEY_LEFT && keydata.action)
+        map->player.angle -= ROTATION_SPEED;
+    else if (keydata.key == MLX_KEY_RIGHT && keydata.action)
+        map->player.angle += ROTATION_SPEED;
+    else if (keydata.key == MLX_KEY_ESCAPE && keydata.action)
+        exit(1);
+    int map_x = new_px / Size;
+    int map_y = new_py / Size;
+    if (map->map_array[map_y][map_x] != '1')
+    {
+        mlx_delete_image(map->mlx, map->player.image_player);
+        map->player.image_player = mlx_new_image(map->mlx, 1920, 1080);
+        map->player.px = new_px;
+        map->player.py = new_py;
+        mlx_put_pixel(map->player.image_player, map->player.px, map->player.py, 0xFF0000FF);
+        draw_line(map->player.image_player, map->player.px, map->player.py, 16, 0xFF0000FF, map->player.angle);
+        mlx_image_to_window(map->mlx, map->player.image_player, 340, 240);
+    }
 }
 
 int main(int ac, char **av)
 {
-	t_map map_struct;
-	int fd = open(av[1], O_RDONLY);
-	if (fd < 0)
-		return (ft_putstrn_fd("Error: Unable to open file", 2), 1);
-	if (check_all(ac, av, &map_struct, fd))
-		return (close(fd), 1);
-	close(fd);
-	check_map(&map_struct);
-	map_struct.player.x = -1;
-	map_struct.player.y = -1;
-	int x = 0, y = 0;
-	while (map_struct.map_array[y])
-	{
-		x = 0;
-		while (map_struct.map_array[y][x])
-		{
-			if (ft_strchr("WESN", map_struct.map_array[y][x]))
-			{
-				if (map_struct.map_array[y][x] == 'N')
-					map_struct.player_dir = 0;
-				else if (map_struct.map_array[y][x] == 'E')
-					map_struct.player_dir = 1;
-				else if (map_struct.map_array[y][x] == 'S')
-					map_struct.player_dir = 2;
-				else if (map_struct.map_array[y][x] == 'W')
-					map_struct.player_dir = 3;
-				map_struct.player.x = x;
-				map_struct.player.y = y;
-				break;
-			}
-			x++;
-		}
-		y++;
-	}
-	map_struct.mlx = mlx_init(1920, 1080, "Cub3D", true);
-	map_struct.image = mlx_new_image(map_struct.mlx, 1920, 1080);
-	draw_map(&map_struct);
-	mlx_image_to_window(map_struct.mlx, map_struct.image, 360, 180);
-	mlx_key_hook(map_struct.mlx, &key_hook, &map_struct);
-	mlx_loop(map_struct.mlx);
-	return (0);
+    t_map map_struct;
+    int fd = open(av[1], O_RDONLY);
+    if (fd < 0)
+        return (ft_putstrn_fd("Error: Unable to open file", 2), 1);
+    if (check_all(ac, av, &map_struct, fd))
+        return (close(fd), 1);
+    close(fd);
+    check_map(&map_struct);
+    map_struct.player.angle = 0;
+    map_struct.player.px = -1;
+    map_struct.player.py = -1;
+    int x = 0, y = 0;
+    while (map_struct.map_array[y])
+    {
+        x = 0;
+        while (map_struct.map_array[y][x])
+        {
+            if (ft_strchr("WESN", map_struct.map_array[y][x]))
+            {
+                if (map_struct.map_array[y][x] == 'N')
+                    map_struct.player.angle = 270 * (M_PI / 180);
+                else if (map_struct.map_array[y][x] == 'E')
+                    map_struct.player.angle = 0;
+                else if (map_struct.map_array[y][x] == 'S')
+                    map_struct.player.angle = 90 * (M_PI / 180);
+                else if (map_struct.map_array[y][x] == 'W')
+                    map_struct.player.angle = 180 * (M_PI / 180);
+                map_struct.player.px = x * Size;
+                map_struct.player.py = y * Size;
+                break;
+            }
+            x++;
+        }
+        y++;
+    }
+    map_struct.mlx = mlx_init(1920, 1080, "Cub3D", true);
+    map_struct.image = mlx_new_image(map_struct.mlx, 1920, 1080);
+    map_struct.player.image_player = mlx_new_image(map_struct.mlx, 1920, 1080);
+    draw_map(&map_struct);
+    mlx_image_to_window(map_struct.mlx, map_struct.image, 340, 240);
+    mlx_image_to_window(map_struct.mlx, map_struct.player.image_player, 340, 240);
+    mlx_key_hook(map_struct.mlx, &key_hook, &map_struct);
+    mlx_loop(map_struct.mlx);
+    return (0);
 }
