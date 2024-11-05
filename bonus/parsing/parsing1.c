1ad3b43a105213fa218f3a691b43dc26c88bdac6
Kenaoui Adam <kadam@c1r6p2.1337.ma>
2024-11-05 18:06:11 +0100

    no SEGV

--- bonus/parsing/parsing1.c
+++ bonus/parsing/parsing1.c
@@ -47,18 +47,45 @@ int	process_path(char **str, int i, char *name, char **path)
 	free_2d_array(split);
 	return (0);
 }
-
-static int	init_struct(t_data *map_struct, t_helper *helper, int fd, int i)
+static int is_new_line(char **line, int *fd, char *av, int j)
 {
-	map_struct->no = NULL;
-	map_struct->so = NULL;
-	map_struct->we = NULL;
-	map_struct->ea = NULL;
+	if (!*line || !line)
+		return (ft_putstrn_fd("Error: Failed to read file", 2), 1);
+	while (*line)
+	{
+		if (*line[0] != '\n')
+		{
+			j = 1;
+			break;
+		}
+		free(*line);
+		*line = get_next_line(*fd);
+	}
+	if (j == 0)
+		return (ft_putstrn_fd("Error: Empty line", 2), 1);
+	close(*fd);
+	*fd = open(av, O_RDONLY);
+	if (*fd == -1)
+		return (ft_putstrn_fd("Error: Failed to open file", 2), 1);
+	*line = get_next_line(*fd);
+	if (!*line)
+		return (ft_putstrn_fd("Error: Failed to read file", 2), 1);
+	return (0);
+}
+
+static int	init_struct(t_data *data_struct, t_helper *helper, int fd, char *av)
+{
+	data_struct->no = NULL;
+	data_struct->so = NULL;
+	data_struct->we = NULL;
+	data_struct->ea = NULL;
+	int i;
+
 	i = 0;
 	while (i < 3)
 	{
-		map_struct->f[i] = -1;
-		map_struct->c[i] = -1;
+		data_struct->f[i] = -1;
+		data_struct->c[i] = -1;
 		i++;
 	}
 	helper->res = 0;
@@ -68,8 +95,8 @@ static int	init_struct(t_data *map_struct, t_helper *helper, int fd, int i)
 	while (i < 6)
 		helper->find[i++] = 0;
 	helper->line = get_next_line(fd);
-	if (!helper->line)
-		return (ft_putstrn_fd("Error: Failed to read file", 2), 1);
+	if (is_new_line(&helper->line, &fd, av, 0))
+		return (1);
 	return (0);
 }
 
@@ -102,7 +129,7 @@ int	check_all(int ac, char **av, t_data *map_struct, int fd)
 	if (ac != 2 || (ac == 2 && av[1] && !check_av_path(av[1], ft_strlen(av[1]),
 				0)))
 		return (ft_putstrn_fd("Error\nInvalid arg\n", 2), 1);
-	if (init_struct(map_struct, &helper, fd, 0))
+	if (init_struct(map_struct, &helper, fd, av[1]))
 		return (1);
 	if (read_file(fd, &helper))
 		return (1);
--- mandatory/parsing/parsing1.c
+++ mandatory/parsing/parsing1.c
@@ -48,12 +48,40 @@ int	process_path(char **str, int i, char *name, char **path)
 	return (0);
 }
 
-static int	init_struct(t_data *data_struct, t_helper *helper, int fd, int i)
+static int is_new_line(char **line, int *fd, char *av, int j)
+{
+	if (!*line || !line)
+		return (ft_putstrn_fd("Error: Failed to read file", 2), 1);
+	while (*line)
+	{
+		if (*line[0] != '\n')
+		{
+			j = 1;
+			break;
+		}
+		free(*line);
+		*line = get_next_line(*fd);
+	}
+	if (j == 0)
+		return (ft_putstrn_fd("Error: Empty line", 2), 1);
+	close(*fd);
+	*fd = open(av, O_RDONLY);
+	if (*fd == -1)
+		return (ft_putstrn_fd("Error: Failed to open file", 2), 1);
+	*line = get_next_line(*fd);
+	if (!*line)
+		return (ft_putstrn_fd("Error: Failed to read file", 2), 1);
+	return (0);
+}
+
+static int	init_struct(t_data *data_struct, t_helper *helper, int fd, char *av)
 {
 	data_struct->no = NULL;
 	data_struct->so = NULL;
 	data_struct->we = NULL;
 	data_struct->ea = NULL;
+	int i;
+
 	i = 0;
 	while (i < 3)
 	{
@@ -68,8 +96,8 @@ static int	init_struct(t_data *data_struct, t_helper *helper, int fd, int i)
 	while (i < 6)
 		helper->find[i++] = 0;
 	helper->line = get_next_line(fd);
-	if (!helper->line)
-		return (ft_putstrn_fd("Error: Failed to read file", 2), 1);
+	if (is_new_line(&helper->line, &fd, av, 0))
+		return (1);
 	return (0);
 }
 
@@ -92,7 +120,7 @@ int	check_all(int ac, char **av, t_data *data_struct, int fd)
 	if (ac != 2 || (ac == 2 && av[1] && !check_av_path(av[1], ft_strlen(av[1]),
 				0)))
 		return (ft_putstrn_fd("Error\nInvalid arg\n", 2), 1);
-	if (init_struct(data_struct, &helper, fd, 0))
+	if (init_struct(data_struct, &helper, fd, av[1]))
 		return (1);
 	if (read_file(fd, &helper))
 		return (1);
