96f378cd0f5fe5ff71d2d67e3e0d7094409fdb36
hi0ki <ansarimehdi518@gmail.com>
2024-11-10 18:37:05 +0100

    push

--- bonus/libft_utils/ft_atoi.c
+++ bonus/libft_utils/ft_atoi.c
@@ -6,11 +6,22 @@
 /*   By: eel-ansa <eel-ansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:25:16 by kadam             #+#    #+#             */
-/*   Updated: 2024/11/08 13:52:17 by eel-ansa         ###   ########.fr       */
+/*   Updated: 2024/11/10 18:36:24 by eel-ansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+
+static int	atoi_helper(const char *str, int i)
+{
+	if (str[i] == '+')
+	{
+		if (str[i + 1] && (str[i + 1] == '-' || str[i + 1] == '+'))
+			return (-1);
+		return (0);
+	}
+	return (2);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -25,12 +36,10 @@ int	ft_atoi(const char *str)
 		i++;
 	if (str[i] == '-')
 		return (-1);
-	if (str[i] == '+')
-	{
-		if (str[i + 1] && (str[i + 1] == '-' || str[i + 1] == '+'))
-			return (-1);
+	if (!atoi_helper(str, i))
 		i++;
-	}
+	else if (atoi_helper(str, i) == -1)
+		return (-1);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		old = result;
--- bonus/libft_utils/ft_strcmp.c
+++ bonus/libft_utils/ft_strcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kadam <kadam@student.1337.ma>              +#+  +:+       +#+        */
+/*   By: eel-ansa <eel-ansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:23:11 by kadam             #+#    #+#             */
-/*   Updated: 2024/09/04 19:28:20 by kadam            ###   ########.fr       */
+/*   Updated: 2024/11/10 17:57:01 by eel-ansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-    if (!s1 || !s2)
-        return (0);
-    int i = 0;
-    while (s1[i] && s2[i] && s1[i] == s2[i])
-        i++;
-    return (s1[i] - s2[i]);
+	int	i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
